7f5a885aa689c499cd6ced769678768e837738ac
Joey Di Nardo <yejodido@gmail.com>
2015-12-31 12:30:38 -0500

    centered language logos

--- src/components/organisms/LanguageCard.js
+++ src/components/organisms/LanguageCard.js
@@ -14,7 +14,9 @@ export default class LanguageCard extends Component {
   render() {
     return (
       <div className={`${styles.LanguageCard}`}>
-        <img className={`${styles.image}`} src={require('src/assets/images/logos/passport.svg')}></img>
+        <div>
+          <img className={`${styles.image}`} src={require('src/assets/images/logos/grunt.svg')}></img>
+        </div>
         <CardName />
       </div>
     );
