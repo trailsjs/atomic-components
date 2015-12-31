e5e0464bf337a5446c7e2ca2cf8eab466f7fe65e
Joey Di Nardo <yejodido@gmail.com>
2015-12-31 18:50:07 -0500

    export consts

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,4 +1,7 @@
 export const SELECT_TASKRUNNER_CARD = 'SELECT_TASKRUNNER_CARD';
+export const SELECT_FRONTEND_CARD = 'SELECT_FRONTEND_CARD';
+export const SELECT_ROUTER_CARD = 'SELECT_ROUTER_CARD';
+export const SELECT_AUTH_CARD = 'SELECT_AUTH_CARD';
 
 export function selectTaskRunnerCard() {
   return {
