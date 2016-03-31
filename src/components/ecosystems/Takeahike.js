9009d54ecab05c7e076ea50e27d6459049b3d5b6
Joey Di Nardo <yejodido@gmail.com>
2016-03-31 18:28:29 -0400

    added floor and positioning

--- src/components/ecosystems/Takeahike.js
+++ src/components/ecosystems/Takeahike.js
@@ -12,20 +12,23 @@ import styles from './styles/Takeahike.scss';
 export default class Takeahike extends Component {
   render() {
     return (
-      <div className={`${styles.Takeahike}`}>
-        <div className={`${styles.flexLeft}`}></div>
-        <div className={`${styles.flexMiddle}`}>
-            <div className={`${styles.outercabinet}`}>
-                <div className={`${styles.innercabinet}`}>
-                    <CardRow type="router"/>
-                    <CardRow type="taskrunner" />
-                    <CardRow type="frontend"/>
-                    <CardRow type="auth"/>
+      <div className={`${styles.wrapper}`}>
+          <div className={`${styles.Takeahike}`}>
+            <div className={`${styles.flexLeft}`}></div>
+            <div className={`${styles.flexMiddle}`}>
+                <div className={`${styles.outercabinet}`}>
+                    <div className={`${styles.innercabinet}`}>
+                        <CardRow type="router"/>
+                        <CardRow type="taskrunner" />
+                        <CardRow type="frontend"/>
+                        <CardRow type="auth"/>
+                    </div>
                 </div>
+                <div className={`${styles.bottomtray}`}></div>
             </div>
-            <div className={`${styles.bottomtray}`}></div>
-        </div>
-        <div className={`${styles.flexRight}`}></div>
+            <div className={`${styles.flexRight}`}></div>
+          </div>
+          <div className={`${styles.floor}`}></div>
       </div>
     );
   }
