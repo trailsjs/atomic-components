42e64e428d263a535bf716ce796d858e824927e5
Joey Di Nardo <yejodido@gmail.com>
2016-04-09 19:42:21 -0400

    moved svg statics to file loaders

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -39,7 +39,10 @@ module.exports = {
       test: /\.(ttf|otf|eot|woff(2)?)(\?[a-z0-9]+)?$/,
       loader: 'file-loader?name=fonts/[name].[ext]'
     }, {
-      test: /\.(png|jpg|svg)$/,
+      test: /\.(svg)$/,
+      loader: 'file-loader?name=svgs/[name].[ext]'
+    }, {
+      test: /\.(png|jpg)$/,
       loader: 'url?limit=25000'
     }]
   }
