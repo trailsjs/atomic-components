8f2de9e37e9a2d9fcc3a27351117a1fd5f0190aa
Travis J. Webb <me@traviswebb.com>
2017-02-12 00:24:44 -0500

    [config] update webpack

--- config/webpack.js
+++ config/webpack.js
@@ -9,45 +9,42 @@ const path = require('path');
 const webpack = require('webpack');
 
 module.exports = {
-  options: {
-    devtool: 'eval',
-    resolve: {
-      root: path.join(__dirname, '../')
-    },
-    entry: [
-      './src/index'
-    ],
-    output: {
-      path: path.join(__dirname, '..', 'static'),
-      filename: 'bundle.js',
-      publicPath: '/static/'
-    },
-    plugins: [
-      new webpack.HotModuleReplacementPlugin(),
-      new webpack.NoErrorsPlugin()
-    ],
-    module: {
-      loaders: [{
-        test: /\.js$/,
-        loader: 'babel',
-        query: {
-          presets: ['react', 'es2015', 'stage-0']
-        }
-      }, {
-        test: /\.scss$/,
-        loaders: [
-          'style',
-          'css?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]',
-          'sass'
-        ]
-      }, {
-        test: /\.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
-        loader: 'file-loader?name=fonts/[name].[ext]'
-      }, {
-        test: /\.(png|jpg)$/,
-        loader: 'url?limit=25000'
-      }]
-    }
+  devtool: 'eval',
+  resolve: {
+    root: path.join(__dirname, '../')
+  },
+  entry: [
+    './src/index'
+  ],
+  output: {
+    path: path.join(__dirname, '..', 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      loader: 'babel',
+      query: {
+        presets: ['react', 'es2015', 'stage-0']
+      }
+    }, {
+      test: /\.scss$/,
+      loaders: [
+        'style',
+        'css?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]',
+        'sass'
+      ]
+    }, {
+      test: /\.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+      loader: 'file-loader?name=fonts/[name].[ext]'
+    }, {
+      test: /\.(png|jpg)$/,
+      loader: 'url?limit=25000'
+    }]
   }
-
 }
